1addbe1312532e1e45c336368bc5df0d318adabf
Jules Lefebvre <jltlefebvre@gmail.com>
2022-11-29 21:41:05 -0500

    feat: global constants

---
+++ common.py
@@ -0,0 +1,10 @@
+# EC2
+UBUNTU_IMAGE = "ami-08c40ec9ead489470"
+US_EAST_1A = "us-east-1a"
+T2_MICRO = "t2.micro"
+
+# Port configuration
+DEFAULT_SECURITY_GROUP_NAME = "default"
+CIDR_IP = "0.0.0.0/0"
+HTTP_PORT = 80
+IP_PROTOCOL = "tcp"
