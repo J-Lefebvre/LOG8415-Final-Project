b01747b8c3a5069a6616ab2f5f6219b72de22e14
Jules Lefebvre <jltlefebvre@gmail.com>
2022-11-30 18:10:43 -0500

    feat: ClusterSetup.ssh_execute to run commands on instances

--- cluster_setup.py
+++ cluster_setup.py
@@ -16,3 +16,22 @@ class ClusterSetup:
         self.ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
         self.key = paramiko.RSAKey.from_private_key_file("key.pem")
 
+    def ssh_execute(self, dns, commands):
+        """_summary_
+
+        Args:
+            dns (_type_): _description_
+            commands (_type_): _description_
+        """
+        # Connect to instance via SSH
+        self.ssh.connect(hostname=dns, username="ubuntu", pkey=self.key)
+
+        # Execute command on instance and unpack stdin, stdout, stderr
+        stdin, stdout, stderr = self.ssh.exec_command(commands)
+
+        # Print instance standard output
+        while True:
+            print(stdout.readline())
+            if stdout.channel.exit_status_ready():
+                break
+
