c0d4ab4aced35b5cbfc596ba17ea0ceea1c95a4e
Jules Lefebvre <jltlefebvre@gmail.com>
2022-12-23 22:03:19 -0500

    feat: added docstrings to main.py

--- main.py
+++ main.py
@@ -3,10 +3,13 @@ from cluster_setup import ClusterSetup
 
 ec2 = InstancesCreator()
 
+# Creates a stand-alone node, a master node and three slave nodes.
+# The nodes are instantiated with the UserData in the /scripts folder
 print('Creating instances...')
 instances = ec2.create_instances()
 print('Instances created.')
 
+# Prints information on each instance
 for instance in instances:
     print(instance)
     print(f"\tid: {instances[instance]['id']}")
@@ -14,9 +17,11 @@ for instance in instances:
     print(f"\tpublic-dns: {instances[instance]['public-dns']}")
     print(f"\tipv4: {instances[instance]['ipv4']}")
 
+# Setup and start the MySQL cluster using the ClusterSetup class
 cs = ClusterSetup(instances)
 cs.start_cluster()
 
+# Benchmark MySQL stand-alone and cluster
 cs.run_benchmark()
 
 # Terminate services
